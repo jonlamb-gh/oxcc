23357d61d63810a119e01b537ea116b88c4edc3c
jonlamb-gh <lamb.jon.io@gmail.com>
2018-08-14 07:36:50 -0700

    WIP Kia Soul Petrol brake module

--- src/brake/kia_soul_petrol/brake_module.rs
+++ src/brake/kia_soul_petrol/brake_module.rs
@@ -1,1 +1,27 @@
+// TODO
+// https://github.com/jonlamb-gh/oscc/tree/devel/firmware/brake/kia_soul_petrol
 
+use board::Board;
+use nucleo_f767zi::hal::can::CanFrame;
+use nucleo_f767zi::hal::prelude::*;
+
+pub struct BrakeModule {}
+
+impl BrakeModule {
+    pub fn new() -> Self {
+        panic!("TODO - Kia Soul Petrol brake module not implemented yet");
+        BrakeModule {}
+    }
+
+    pub fn init_devices(&self, board: &mut Board) {
+        board.brake_spoof_enable().set_low();
+        board.brake_light_enable().set_low();
+        // TODO - PIN_DAC_CHIP_SELECT, HIGH
+    }
+
+    pub fn check_for_faults(&mut self, _board: &mut Board) {}
+
+    pub fn publish_brake_report(&mut self, _board: &mut Board) {}
+
+    pub fn process_rx_frame(&mut self, _can_frame: &CanFrame, _board: &mut Board) {}
+}
